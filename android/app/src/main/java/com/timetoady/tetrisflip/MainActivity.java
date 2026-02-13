2e2ba317cfa6d8aa9ebe481ccc825e9415dae9b3
Adam Andreason <adamandreason@gmail.com>
2026-02-12 19:41:33 -0700

    Fix Android immersive fullscreen for gameplay

--- android/app/src/main/java/com/timetoady/tetrisflip/MainActivity.java
+++ android/app/src/main/java/com/timetoady/tetrisflip/MainActivity.java
@@ -1,5 +1,48 @@
 package com.timetoady.tetrisflip;
 
+import android.os.Bundle;
+import android.view.Window;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        enableImmersiveFullscreen();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        enableImmersiveFullscreen();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            enableImmersiveFullscreen();
+        }
+    }
+
+    private void enableImmersiveFullscreen() {
+        Window window = getWindow();
+        WindowCompat.setDecorFitsSystemWindows(window, false);
+
+        WindowInsetsControllerCompat controller =
+            WindowCompat.getInsetsController(window, window.getDecorView());
+        if (controller == null) {
+            return;
+        }
+
+        controller.setSystemBarsBehavior(
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        );
+        controller.hide(WindowInsetsCompat.Type.systemBars());
+    }
+}

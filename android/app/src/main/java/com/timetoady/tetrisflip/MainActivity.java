6208e6750fd999602447dc0c15d41fc01700627a
Adam Andreason <adamandreason@gmail.com>
2026-02-12 19:47:08 -0700

    Fix MainActivity onResume override visibility

--- android/app/src/main/java/com/timetoady/tetrisflip/MainActivity.java
+++ android/app/src/main/java/com/timetoady/tetrisflip/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         enableImmersiveFullscreen();
     }
